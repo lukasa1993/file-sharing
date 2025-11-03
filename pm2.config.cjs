b00c8b21e880b2120f23779f46a15097f3618259
luka dodelia <luka@lnh.ge>
2025-11-03 21:46:09 +0400

    Update pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-	title: "app", // Name of your application
-	script: "index.ts", // Entry point of your application
+	title: "File Sharing", // Name of your application
+	script: "server.ts", // Entry point of your application
 	interpreter: "bun", // Bun interpreter
 	env: {
 		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
